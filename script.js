8aa4ae6ab34006b125053a0f93734bfd26916ace
Florencia De Vuono <florenciadevuono@Florencias-MacBook-Pro-3.local>
2017-07-29 12:48:49 -0300

    New quotes
    Added new quotes

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 var quotes = [
-    "Owning our story can be hard but not nearly as difficult as spending our lives running from it. Embracing our vulnerabilities is risky but not nearly as dangerous as giving up on love and belonging and joy—the experiences that make us the most vulnerable. Only when we are brave enough to explore the darkness will we discover the infinite power of our light.",
-    "We cultivate love when we allow our most vulnerable and powerful selves to be deeply seen and known, and when we honor the spiritual connection that grows from that offering with trust, respect, kindness and affection.",
-    "You are imperfect, you are wired for struggle, but you are worthy of love and belonging.",
-    "Courage starts with showing up and letting ourselves be seen.",
-    "Vulnerability is the birthplace of innovation, creativity and change.",
-    "Because true belonging only happens when we present our authentic, imperfect selves to the world, our sense of belonging can never be greater than our level of self-acceptance.",
-    "Vulnerability sounds like truth and feels like courage. Truth and courage aren't always comfortable, but they're never weakness.",
-    "If we can share our story with someone who responds with empathy and understanding, shame cannot survive.",
-    "The dark does not destroy the light; it defines it. It's our fear of the dark that casts our joy into the shadows.",
-    "Faith is a place of mystery, where we find the courage to believe in what we cannot see and the strength to let go of our fear of uncertainty.",
-    "If you trade your authenticity for safety, you may experience the following: anxiety, depression, eating disorders, addiction, rage, blame, resentment, and inexplicable grief.",
-    "To love someone fiercely, to believe in something with your whole heart, to celebrate a fleeting moment in time, to fully engage in a life that doesn’t come with guarantees – these are risks that involve vulnerability and often pain. But, I’m learning that recognizing and leaning into the discomfort of vulnerability teaches us how to live with joy, gratitude and grace.",
-    "Perfectionism is a self destructive and addictive belief system that fuels this primary thought: If I look perfect, and do everything perfectly, I can avoid or minimize the painful feelings of shame, judgment, and blame.",
-    "If you want to make a difference, the next time you see someone being cruel to another human being, take it personally. Take it personally because it is personal!",
-    "Healthy striving is self-focused: 'How can I improve?'. Perfectionism is other-focused: 'What will they think?'"
+    "Only when we are brave enough to explore the darkness will we discover the infinite power of our light. ~Brene Brown",
+    "We cultivate love when we allow our most vulnerable and powerful selves to be deeply seen and known, and when we honor the spiritual connection that grows from that offering with trust, respect, kindness and affection. ~Brene Brown",
+    "You are imperfect, you are wired for struggle, but you are worthy of love and belonging. ~Brene Brown",
+    "Courage starts with showing up and letting ourselves be seen. ~Brene Brown",
+    "Vulnerability is the birthplace of innovation, creativity and change. ~Brene Brown",
+    "Because true belonging only happens when we present our authentic, imperfect selves to the world, our sense of belonging can never be greater than our level of self-acceptance. ~Brene Brown",
+    "Vulnerability sounds like truth and feels like courage. Truth and courage aren't always comfortable, but they're never weakness. ~Brene Brown",
+    "If we can share our story with someone who responds with empathy and understanding, shame cannot survive. ~Brene Brown",
+    "The dark does not destroy the light; it defines it. It's our fear of the dark that casts our joy into the shadows. ~Brene Brown",
+    "Faith is a place of mystery, where we find the courage to believe in what we cannot see and the strength to let go of our fear of uncertainty. ~Brene Brown",
+    "If you trade your authenticity for safety, you may experience the following: anxiety, depression, eating disorders, addiction, rage, blame, resentment, and inexplicable grief. ~Brene Brown",
+    "Perfectionism is a self destructive and addictive belief system that fuels this primary thought: If I look perfect, and do everything perfectly, I can avoid or minimize the painful feelings of shame, judgment, and blame. ~Brene Brown",
+    "If you want to make a difference, the next time you see someone being cruel to another human being, take it personally. Take it personally because it is personal! ~Brene Brown",
+    "Healthy striving is self-focused: 'How can I improve?'. Perfectionism is other-focused: 'What will they think?'. ~Brene Brown",
+    "It is not the critic who counts; not the man who points out how the strong man stumbles, or where the doer of deed could have done them better. The credit belongs to the man who is actually in the arena, whose face is married by dust and sweat and blood; who strives valiantly... who at the best knows in the end the triumph of high achievement, and who at the worst, if he fails, at least fails while daring greatly. ~Theodore Roosevelt",
+    "Do not fear to be eccentric in opinion, for every opinion now accepted was once eccentric. ~Bertrand Russell"
 ];
 
 var currentIndex;
